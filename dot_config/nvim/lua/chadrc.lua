cc7ad067db68352ff761ade1ae9987da00ac386e
Darius-Ștefan Țacu <88737250+stefanFortza@users.noreply.github.com>
2024-11-28 19:47:59 +0200

    Update docker-compose

--- dot_config/nvim/lua/chadrc.lua
+++ dot_config/nvim/lua/chadrc.lua
@@ -7,6 +7,7 @@ local M = {}
 
 M.base46 = {
   theme = "catppuccin",
+  theme_toggle = { "catppuccin", "catppuccin" },
   -- flavour = "mocha",
   -- hl_override = {
   -- 	Comment = { italic = true },
